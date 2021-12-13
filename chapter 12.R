7a19d182642881d77c0e1c1d115ca0dc27c9970d
yaozhaostats <yzhao28@student.gsu.edu>
2021-12-13 15:58:35 -0500

    Update chapter 12.R

--- chapter 12.R
+++ chapter 12.R
@@ -85,3 +85,48 @@ plot(x, col = cl$cluster, main="clusters from k-means")
 points(cl$centers, col = 1:3, pch = 8, cex=2)
 
 # Example 12.13 (A model based clustering of the iris data)
+
+library(mclust)
+irisBIC <- mclustBIC(iris[,-5], modelNames = "VII") # in the textbook page 705, three forms of Sigma_k is EII, VII, not sure about the third type.
+irisBIC # Notice that there are mistakes in the textbook. The software doesn't choose K=3. And when K=7, BIC is -698.5.
+
+# Let's use K=3 from the textbook:
+mc <- Mclust(iris[,-5], modelNames = "VII", G = 3)
+mc$parameters[2] # the same as estimated centers in the textbook
+mc$parameters$variance$scale # estimated variance-covariance scale factors 
+mc$parameters$pro # the estimated mixing proportions
+mc$BIC 
+
+# start page 706 part in the textbook:
+mc <- Mclust(iris[,-5])
+mc # R returns VEV,2. VEV is corresponding to the "unconstrained covariances", 2 means 2 clusters.
+# Notice that in the textbook, we say 2 clusters. In R, it says 2 mixture components.
+
+mc$parameters$pro  # the estimated mixing probabilities
+mc$parameters[2] # the same as estimated group centers in the textbook
+mc$parameters$variance$sigma # the two estimated covariance matrices are different
+
+# Now we replicate Figure 12.13. 
+# Notice that this figure has 2 clusters (squares and triangles) in each subfigure. So the textbook has a mistake here. The textbook used 2 clusters.
+library(tidyverse)
+pairs(mc$data, pch = mc$classification)
+# If you want to change the shapes, you can map the classification assignment to the desired shape: https://www.rdocumentation.org/packages/graphics/versions/3.6.2/topics/points.
+
+
+# Example 12.16
+
+univ <- read.table("http://users.stat.umn.edu/~sandy/courses/8053/Data/Wichern_data/T12-9.DAT", row.names=1)
+univ.dist <- dist(scale(univ))
+univ.cmd <- cmdscale(univ.dist, 2) # cmdscale: Classical (Metric) Multidimensional Scaling
+
+plot(univ.cmd, type="n", xlab="Dimension 1", ylab="Dimension 2",
+     main="cmdscale(university)")
+text(univ.cmd, labels=rownames(univ.cmd), cex=.5) # different from textbook
+
+
+
+library(MASS)
+univ.sm <- sammon(univ.dist) # Sammon's Non-Linear Mapping
+plot(univ.sm$points, type="n", xlab="Dimension 1", ylab="Dimension 2",
+     main="sammon(university)")
+text(univ.sm$points, labels=rownames(univ), cex=.5)
